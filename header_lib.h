3cacb33e8fd16f19df03230ad420e62ef09a7d60
Nikita Krenov <nikitabaram@gmail.com>
2024-11-05 19:15:36 +0100

    ### Summary of Changes
    The primary changes involve the addition of new header files to the `PasswordManager` project, the organization of these files within the Visual Studio solution explorer, and the inclusion of `#pragma once` directives to prevent multiple inclusions. Additionally, a central header file (`header_lib.h`) has been updated to include several standard library headers and the use of the `std` namespace.
    
    ### List of Changes
    
    1. **Addition of Header Files to Project**:
       - Several header files (`audit_log.h`, `backup_manager.h`, `category_manager.h`, `database.h`, `encryption.h`, `header_lib.h`, `password_manager.h`, `ui.h`, `user.h`) have been added to the `PasswordManager.vcxproj` project file within a new `<ItemGroup>` element.
       - **Reference**: `PasswordManager.vcxproj`
    
    2. **Creation of Filters and Organization of Header Files**:
       - New filters (`lib`, `domain`, `domain\entities`, `domain\repository`) have been created in the `PasswordManager.vcxproj.filters` file.
       - The newly added header files have been associated with these filters to organize the project files within the Visual Studio solution explorer.
       - **Reference**: `PasswordManager.vcxproj.filters`
    
    3. **Addition of `#pragma once` Directive**:
       - The `#pragma once` directive has been added to the top of the following header files to ensure they are included only once during compilation: `audit_log.h`, `backup_manager.h`, `category_manager.h`, `database.h`, `encryption.h`, `password_manager.h`, `ui.h`, `user.h`.
       - **Reference**: `audit_log.h`, `backup_manager.h`, `category_manager.h`, `database.h`, `encryption.h`, `password_manager.h`, `ui.h`, `user.h`
    
    4. **Update to Central Header File**:
       - The `header_lib.h` file has been updated to include multiple standard library headers (`<iostream>`, `<string>`, `<vector>`, `<algorithm>`, `<ctime>`, `<cstdlib>`, `<fstream>`, `<map>`, `<set>`, `<list>`, `<queue>`, `<stack>`, `<deque>`, `<iterator>`, `<numeric>`, `<functional>`) and the use of the `std` namespace.
       - **Reference**: `header_lib.h`

---
+++ header_lib.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <map>
+#include <set>
+#include <list>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <iterator>
+#include <numeric>
+#include <functional>
+
+using namespace std;
